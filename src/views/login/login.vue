9a56579fcc6e466ed4f0df7ea781a8fa98930613
xlearns <2447684095@qq.com>
2022-05-17 02:54:36 +0800

    fix

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,26 +1,6 @@
-<script setup lang="ts">
-import { onMounted } from "vue";
-import { usePermission } from "@/hooks/usePermission";
-import { useStorage } from "@vueuse/core";
-const { changeRole, hasPermission } = usePermission();
-
-function change(key: string) {
-  changeRole(key);
-  location.reload();
-}
-</script>
-
+<script setup lang="ts"></script>
 <template>
   <div class="test"></div>
-  <el-button @click="change('super')">super</el-button>
-  <el-button @click="change('test')">test</el-button>
-  <br />
-  <!-- 需要刷新才会生效 -->
-  <span v-permission="'super'">super</span>
-  <span v-permission="'test'">test</span>
-  <!-- 不需要刷新才会生效 -->
-  <span v-if="hasPermission('super')">1</span>
-  <span v-if="hasPermission('test')">2</span>
 </template>
 <style scoped>
 @import url("@/style/login.scss");
