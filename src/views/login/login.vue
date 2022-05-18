a8503d425bd9255f0382f337641e84229ca5edb7
xlearns <2447684095@qq.com>
2022-05-18 21:46:08 +0800

    login

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { bg, currentWeek } from "./utils/static";
+</script>
 <template>
-  <div class="test"></div>
+  <img :src="bg" class="fixed h-[100%] left-0 bottom-0 -z-1" />
+  <div class="w-[100vw] h-[100vh] grid grid-cols-2">
+    <div class="flex justify-end items-center">
+      <component :is="currentWeek" />
+    </div>
+  </div>
 </template>
-<style scoped>
-@import url("@/style/login.scss");
-</style>
---
+++ src/views/login/utils/static.ts
@@ -0,0 +1,33 @@
+import { computed } from "vue";
+import bg from "@/assets/login/bg.png";
+import avatar from "@/assets/login/avatar.svg";
+import illustration0 from "@/assets/login/illustration0.svg";
+import illustration1 from "@/assets/login/illustration1.svg";
+import illustration2 from "@/assets/login/illustration2.svg";
+import illustration3 from "@/assets/login/illustration3.svg";
+import illustration4 from "@/assets/login/illustration4.svg";
+import illustration5 from "@/assets/login/illustration5.svg";
+import illustration6 from "@/assets/login/illustration6.svg";
+
+const currentWeek = computed(() => {
+  switch (String(new Date().getDay())) {
+    case "0":
+      return illustration0;
+    case "1":
+      return illustration1;
+    case "2":
+      return illustration2;
+    case "3":
+      return illustration3;
+    case "4":
+      return illustration4;
+    case "5":
+      return illustration5;
+    case "6":
+      return illustration6;
+    default:
+      return illustration4;
+  }
+});
+
+export { bg, currentWeek };
