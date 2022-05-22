4d3e60884a889efd15896ca9f8bc6d8912d95bf2
xlearns <2447684095@qq.com>
2022-05-22 16:43:04 +0800

    fix

--- src/layout/header/MultipleHeader.vue
+++ src/layout/header/MultipleHeader.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import LayoutSideBar from "../sider/index.vue";
 import Settings from "./component/Config.vue";
+import { useTheme } from "@/hooks/useTheme";
+let { navTheme } = useTheme();
 </script>
 
 <template>
-  <el-header class="bg-[blue] flex items-center justify-end">
+  <el-header
+    :class="[navTheme == 0 ? 'justify-end' : 'justify-between']"
+    class="bg-[blue] flex items-center"
+  >
+    <LayoutSideBar mode="horizontal" v-if="navTheme == 1" />
     <Settings />
   </el-header>
 </template>
--- src/layout/sider/index.vue
+++ src/layout/sider/index.vue
@@ -1,10 +1,43 @@
 <script setup lang="ts">
 import { useMenuSetting } from "@/hooks/useMenuSetting";
+import { computed } from "vue";
+import { useTheme } from "@/hooks/useTheme";
+let { navTheme } = useTheme();
+defineProps({
+  mode: {
+    type: String,
+    default: () => {
+      return "vertical";
+    }
+  }
+});
 const { getMenuWidth } = useMenuSetting();
+const menuWidth = computed(() => {
+  if (navTheme.value == 0) return getMenuWidth + "px";
+  return "50%";
+});
 </script>
 
 <template>
-  <el-aside :width="getMenuWidth" class="bg-[red]">Aside</el-aside>
+  <el-aside :width="menuWidth" class="h-full">
+    <el-menu class="h-full overflow-hidden" :mode="mode">
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-sub-menu index="2-4">
+          <template #title>item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="3" disabled>Info</el-menu-item>
+      <el-menu-item index="4">Orders</el-menu-item>
+    </el-menu>
+  </el-aside>
 </template>
 
 <style scoped></style>
