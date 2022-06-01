eb889cbfe42c35dc397e315f72af0fc624689ac1
xlearns <2447684095@qq.com>
2022-06-02 02:35:05 +0800

    fix(router) router transition animate

--- src/layout/content/index.vue
+++ src/layout/content/index.vue
@@ -7,22 +7,37 @@
         <!-- todo:
              缓存
         -->
-        <transition name="route-animate" mode="out-in">
-          <component :is="Component" :key="route.fullPath" />
+        <transition name="route-animate" mode="out-in" appear>
+          <component
+            :is="Component"
+            :key="route.fullPath"
+            class="main-content"
+          />
         </transition>
       </template>
     </RouterView>
   </el-main>
 </template>
+<!-- <style scoped>
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
+</style> -->
 <style scoped>
 .route-animate-enter-active,
 .route-animate-leave-active {
-  opacity: 1;
-  transition: opacity 0.4s ease-in;
+  transition: all 0.5s;
 }
 
-.route-animate-enter,
+.route-animate-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
 .route-animate-leave-to {
   opacity: 0;
+  transform: translateX(30px);
+}
+
+.main-content {
+  margin: 24px;
 }
 </style>
