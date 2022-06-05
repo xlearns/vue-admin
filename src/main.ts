0ef1c7a6d86487289f519bcf94e374c825712ffe
xlearns <2447684095@qq.com>
2022-06-06 03:19:38 +0800

    fix(vite): fix vite load slow for element plus

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import App from "./App.vue";
 import router from "./router";
 import { MotionPlugin } from "@vueuse/motion";
 // element plus按需引入会导致 vite启动巨慢
-import { useElementPlus } from "./plugins/element-plus";
-// import ElementPlus from "element-plus";
+// import { useElementPlus } from "./plugins/element-plus";
+import ElementPlus from "element-plus";
 import directives from "@/directives";
 // 导入公共样式
 import "./style/index.scss";
-// import "element-plus/dist/index.css";
+import "element-plus/dist/index.css";
 import { setupRouterGuard } from "@/router/guard";
 // 国际化
 import { useI18n } from "./plugins/i18n";
@@ -21,12 +21,12 @@ import "vue3-contextmenu/dist/vue3-contextmenu.css";
 const app = createApp(App);
 
 Object.keys(directives).forEach(name => app.directive(name, directives[name]));
-// app.use(ElementPlus);
+app.use(ElementPlus);
 app.use(MotionPlugin);
 app.use(createPinia());
 app.use(contextmenu);
 app.use(router);
 setupRouterGuard(router);
-app.use(useElementPlus);
+// app.use(useElementPlus);
 app.use(useI18n);
 app.mount("#app");
