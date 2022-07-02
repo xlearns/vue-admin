e06beae50de557be5b97b326ba3c478d364a60dc
xlearns <2447684095@qq.com>
2022-07-02 22:33:32 +0800

    refactor(search): refactor search add empty

--- src/layout/header/component/search/search.vue
+++ src/layout/header/component/search/search.vue
@@ -6,10 +6,14 @@ defineProps({
     default: false
   }
 });
+const search = ref("");
 </script>
 
 <template>
   <div>
-    <el-input placeholder="请输入关键词搜索" />
+    <el-input placeholder="请输入关键词搜索" v-model="search" />
+    <div class="container">
+      <el-empty v-if="true" description="暂无搜索结果" />
+    </div>
   </div>
 </template>
