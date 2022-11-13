024800f1e1b35a1f89f5d0da3ddc01d8f5a18632
xlearns <2447684095@qq.com>
2022-11-13 23:12:44 +0800

    feat: add backtop for all

--- src/layout/content/index.vue
+++ src/layout/content/index.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 import { useMainElement } from "@/hooks/useMainElement";
+// add ?component fix ts type error
+import BackTop from "@/assets/svg/back_top.svg?component";
 const { el } = useMainElement();
 </script>
 
 <template>
-  <el-main ref="el" class="bg-[#f0f2f5] h-[100vh] !overflow-hidden">
+  <el-main
+    ref="el"
+    class="main-container bg-[#f0f2f5] h-[100vh] !overflow-hidden"
+  >
     <el-scrollbar>
       <div class="p-[20px] h-full w-full box-border">
         <RouterView>
           <template #default="{ Component, route }">
-            <!-- todo:
-             缓存
-        -->
+            <el-backtop
+              title="回到顶部"
+              target=".main-container .el-scrollbar__wrap"
+            >
+              <BackTop />
+            </el-backtop>
+            <!-- todo: 缓存 -->
             <transition name="route-animate" mode="out-in" appear>
               <component :is="Component" :key="route.fullPath" />
             </transition>
